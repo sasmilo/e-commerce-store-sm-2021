88360e032129023d159286b641223ea1b792b0b7
Sasa Milosevic <sasmilo20@gmail.com>
2021-03-09 09:45:10 +0100

    Modify cypress tests

--- cypress/integration/shoppingFlow.spec.js
+++ cypress/integration/shoppingFlow.spec.js
@@ -1,3 +1,92 @@
-import { ShoppingFlow } from './shoppingFlowComponent.spec';
+// import { ShoppingFlow } from './shoppingFlowComponent.spec';
 
-ShoppingFlow();
+// ShoppingFlow();
+
+// export function ShoppingFlow() {
+describe('ShoppingFlow', () => {
+  it('Add to cart, change quantity and remove from cart', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('[data-cy="header-products"]')
+      // ...and click on that element
+
+      .click();
+
+    cy.get('[data-cy=product-17]').click();
+
+    cy.get('[data-cy="button-add-to-cart"]').click();
+
+    cy.get('[data-cy="product-page-amount-in-cart"]').should(
+      'contain.text',
+      '1',
+    );
+
+    cy.get('[data-cy="header-items-in-the-cart"]').should('contain.text', '1');
+
+    cy.get('[data-cy="button-add-to-cart"]').click();
+
+    cy.get('[data-cy="product-page-amount-in-cart"]').should(
+      'contain.text',
+      '2',
+    );
+
+    cy.get('[data-cy="header-items-in-the-cart"]').should('contain.text', '2');
+
+    cy.get('[data-cy="button-remove-from-cart"]').click();
+
+    cy.get('[data-cy="product-page-amount-in-cart"]').should(
+      'contain.text',
+      '1',
+    );
+
+    cy.get('[data-cy="header-items-in-the-cart"]').should('contain.text', '1');
+
+    cy.get('[data-cy="header-products"]')
+      // ...and click on that element
+
+      .click();
+
+    cy.get('[data-cy=product-16]').click();
+
+    cy.get('[data-cy="button-add-to-cart"]').click();
+
+    cy.get('[data-cy="product-page-amount-in-cart"]').should(
+      'contain.text',
+      '1',
+    );
+    cy.get('[data-cy="header-items-in-the-cart"]').should('contain.text', '2');
+
+    cy.get('[data-cy="button-remove-from-cart"]').click();
+
+    cy.get('[data-cy="product-page-amount-in-cart"]').should(
+      'contain.text',
+      '0',
+    );
+
+    cy.get('[data-cy="header-items-in-the-cart"]').should('contain.text', '1');
+
+    cy.get('[data-cy="link-go-to-cart"]').click();
+
+    cy.get('[data-cy="cart-button-add-to-cart"]').click();
+
+    cy.get('[data-cy="amount-of-one-product-in-cart"]').should(
+      'contain.text',
+      '2',
+    );
+
+    cy.get('[data-cy="header-items-in-the-cart"]').should('contain.text', '2');
+    cy.get('[data-cy="cart-button-remove-from-cart"]').click();
+
+    cy.get('[data-cy="amount-of-one-product-in-cart"]').should(
+      'contain.text',
+      '1',
+    );
+
+    cy.get('[data-cy="header-items-in-the-cart"]').should('contain.text', '1');
+
+    cy.get('[data-cy="go-back-to-shop"]').click();
+  });
+});
+
+// Cypress.Cookies.preserveOnce('cart');
+// }
