8aa0abd039f0a5181d97eecc9536d4d363c6097f
Sasa Milosevic <sasmilo20@gmail.com>
2021-04-26 15:25:24 +0200

    Simplify code on the thankyou page

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -1,50 +1,14 @@
-import { GetServerSidePropsContext } from 'next';
+import Cookies from 'js-cookie';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../components/Layout';
-import { getProductInformation } from '../util/database';
 
-type CookieProduct = {
-  id: number;
-  quantity: number;
-};
-
-type DBProduct = {
-  id: number;
-  category: string;
-  productName: string;
-  productPrice: number;
-  description: string;
-  productImage: string;
-  productStock: number;
-  productSize: string;
-  productColor: string;
-  productTags: string;
-};
-
-type FinalShoppingCart = {
-  id: number;
-  category: string;
-  productName: string;
-  productPrice: number;
-  description: string;
-  productImage: string;
-  productStock: number;
-  productSize: string;
-  productColor: string;
-  productTags: string;
-  quantity: number;
-};
-
-type Props = {
-  finalShoppingCart: FinalShoppingCart[];
-};
-
-export default function ThankYou(props: Props) {
-  const cart = props.finalShoppingCart;
+export default function ThankYou() {
+  const cart = Cookies.get('cart');
+  const cartCookieObject = cart ? JSON.parse(cart) : [];
   return (
-    <Layout finalShoppingCart={cart}>
+    <Layout finalShoppingCart={cartCookieObject}>
       <Head>
         <title>Thank you</title>
       </Head>
@@ -63,30 +27,4 @@ export default function ThankYou(props: Props) {
       </Link>
     </Layout>
   );
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const cart = context.req.cookies.cart;
-  const cartCookieObject = cart ? JSON.parse(cart) : [];
-
-  const products = await getProductInformation();
-
-  const finalShoppingCart = cartCookieObject.map(
-    (cookieProduct: CookieProduct) => {
-      return {
-        ...products.find(
-          (product: DBProduct) => cookieProduct.id === product.id,
-        ),
-        quantity: cookieProduct.quantity,
-      };
-    },
-  );
-
-  return {
-    props: {
-      cartCookieObject: cartCookieObject,
-      products: products,
-      finalShoppingCart: finalShoppingCart,
-    },
-  };
 }
